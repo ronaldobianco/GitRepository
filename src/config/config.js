b46a417696ee8c9cb75becafd5f8f859243a554f
Ronaldo-Bianco-Caiena <ronaldo.bianco@caiena.net>
2022-03-14 20:36:20 -0300

    config files

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.BD_NAME , process.env.BD_USER , process.env.BD_PASSWORD , {
   dialect: 'postgres',
-  hots:process.env.HOTS
+
 })
 sequelize.sync()
 module.exports = sequelize;
